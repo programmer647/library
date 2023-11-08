32a17245b3782f164ca33acca873ab8493b0ec19
programmer647 <bourne.scm@oundleschool.org.uk>
2023-11-08 09:25:14 +0000

    added admin home and browse books page

--- install.php
+++ install.php
@@ -1,7 +1,7 @@
 <?php
 include_once("connection.php")
 
-$stmt1=$conn->prepare("DROP TABLE IF EXISTS Tblusers;
+$stmt=$conn->prepare("DROP TABLE IF EXISTS Tblusers;
 CREATE TABLE Tblusers
 (UserID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 Username VARCHAR(20) NOT NULL,
@@ -11,21 +11,21 @@ Password VARCHAR (200) NOT NULL,
 Year INT(2) NOT NULL,
 House VARCHAR(20) NOT NULL,
 Role TINYINT(1))");
-$stmt1->execute();
-$stmt1->closeCursor();
+$stmt->execute();
+$stmt->closeCursor();
 
-$stmt2 = $conn->prepare("DROP TABLE IF EXISTS TblUser;
-CREATE TABLE TblUser 
+$stmt=$conn->prepare("DROP TABLE IF EXISTS Tblbooks;
+CREATE TABLE Tblusers
 (UserID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 Username VARCHAR(20) NOT NULL,
 Surname VARCHAR(20) NOT NULL,
 Forename VARCHAR(20) NOT NULL,
-Password VARCHAR(200) NOT NULL,
+Password VARCHAR (200) NOT NULL,
 Year INT(2) NOT NULL,
-Balance DECIMAL(15,2) NOT NULL,
+House VARCHAR(20) NOT NULL,
 Role TINYINT(1))");
-$stmt2->execute();
-$stmt2->closeCursor(); 
+$stmt->execute();
+$stmt->closeCursor();
 
 
 ?>
---
+++ loginprocess.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+print_r($_POST);
+include_once ("connection.php");
+array_map("htmlspecialchars", $_POST);
+$stmt = $conn->prepare("SELECT * FROM tblusers;" );
+$stmt->bindParam(':username', $_POST['Username']);
+$stmt->execute();
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+{ 
+    $hashed = $row['Password'];
+    $attempt= $_POST['Pword'];
+    if(password_verify($attempt, $hashed)){
+        //header('Location: users.php');
+        $_SESSION['name']=$row['Surname'];
+        echo("qqqq");
+    }else{
+        echo("SDES");
+        //header('Location: login.php');
+    }
+}
+$conn=null;
+?>
