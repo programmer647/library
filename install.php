47e61443ca4731c925d227bb9632209dcbf26be2
programmer647 <bourne.scm@oundleschool.org.uk>
2023-11-08 10:10:34 +0000

    changed install page and add books pages

--- install.php
+++ install.php
@@ -1,5 +1,5 @@
 <?php
-include_once("connection.php")
+include_once("connection.php");
 
 $stmt=$conn->prepare("DROP TABLE IF EXISTS Tblusers;
 CREATE TABLE Tblusers
@@ -13,17 +13,24 @@ House VARCHAR(20) NOT NULL,
 Role TINYINT(1))");
 $stmt->execute();
 $stmt->closeCursor();
+$hashed_password = password_hash("password", PASSWORD_DEFAULT);
+$stmt = $conn->prepare("INSERT INTO Tblusers(UserID,Username,Surname,Forename,Password,Year,House,Role)VALUES
+(NULL,'Jsmith','Smith','John',:hp,10,'Laxton',0),
+(NULL,'Rcunniffe','Cunniffe','Robert',:hp,0,'St A',1),
+(NULL,'Hbass','Hector','Bass',:hp,10,'St A',0)
+");
+$stmt->bindParam(':hp', $hashed_password);
+$stmt->execute();
+$stmt->closeCursor();
+ 
+
 
 $stmt=$conn->prepare("DROP TABLE IF EXISTS Tblbooks;
-CREATE TABLE Tblusers
-(UserID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-Username VARCHAR(20) NOT NULL,
-Surname VARCHAR(20) NOT NULL,
-Forename VARCHAR(20) NOT NULL,
-Password VARCHAR (200) NOT NULL,
-Year INT(2) NOT NULL,
-House VARCHAR(20) NOT NULL,
-Role TINYINT(1))");
+CREATE TABLE Tblbooks
+(ISBN INT(14) NOT NULL,
+Title VARCHAR(20) NOT NULL,
+Author_firstname VARCHAR(20) NOT NULL,
+Author_surname VARCHAR(20) NOT NULL)");
 $stmt->execute();
 $stmt->closeCursor();
 
