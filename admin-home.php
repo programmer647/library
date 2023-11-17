2f735af57b36a6be58d9002215319e6c599f8f61
programmer647 <bourne.scm@oundleschool.org.uk>
2023-11-17 14:17:52 +0000

    changes

--- admin-home.php
+++ admin-home.php
@@ -23,6 +23,23 @@ if ($_SESSION['role']!=2)
 <body>
 
     <h1>Admin home page</h1>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">OSLibrary</a>
+          </div>
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="home-page.html">Home</a></li>
+            <li><a href="books.php">Add books</a></li>
+            <li><a href="users.php">Add users</a></li>
+            <li><a href="#">View overdue books</a></li>
+            <li><a href="viewusers.php">View users</a></li>
+            <li><a href="#">Loan books to students</a></li>
+
+            <li><a href="#">Page 3</a></li>
+          </ul>
+        </div>
+      </nav>
 
 </body>
 
