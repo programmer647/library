22d28e58cedc70a479d0e294de7e04f48c0ee1c2
programmer647 <bourne.scm@oundleschool.org.uk>
2024-01-03 18:01:38 +0000

    finished overdue page

--- overdue.php
+++ overdue.php
@@ -3,14 +3,23 @@
 include_once("connection.php");
 include_once("checkoverdue.php");
 
-$stmt = $conn->prepare("SELECT * FROM tblloans WHERE Overdue=True;" );
+$stmt = $conn->prepare("SELECT LoanID as loanid, tblbooks.Title as t, tblusers.Forename as f, tblusers.Surname as s, Date_due as d FROM tblloans
+INNER JOIN tblbooks 
+ON tblbooks.ISBN=tblloans.ISBN
+INNER JOIN tblusers 
+ON tblusers.UserID=tblloans.UserID where Overdue=True") ;
 $stmt->execute();
 
 print_r($row);
 
+
+echo("Overdue books:");
+echo('<br>');
+
 while ($row = $stmt ->fetch(PDO::FETCH_ASSOC))
 {
-    echo($row['UserID'].', '.$row['ISBN'].', '.$row['Date_loaned'].', '.$row['Date_due']);
+    echo($row['t'].', '.$row['f'].', '.$row['s'].', '.$row['d']);
+    echo('</br>');
 }
 
 
